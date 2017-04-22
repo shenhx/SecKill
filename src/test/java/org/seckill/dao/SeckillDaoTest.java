f75cfddc99c5e84c5b24a36ceae00a71dfc983dd
shenhx <hongxiao_shen@kingdee.com>
2017-04-22 23:51:44 +0800

    正常调通

--- src/test/java/org/seckill/dao/SeckillDaoTest.java
+++ src/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by JavaDev on 2017/4/22.
@@ -14,27 +16,37 @@ import javax.annotation.Resource;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/spring-dao.xml"})
 public class SeckillDaoTest {
+    //注入Dao实现类依赖
     @Resource
     private SeckillDao seckillDao;
 
+
     @Test
-    public void testQueryById() throws Exception{
-        long id = 1000;
-        Seckill seckill = seckillDao.queryById(id);
+    public void queryById() throws Exception {
+        long seckillId=1000;
+        Seckill seckill=seckillDao.queryById(seckillId);
         System.out.println(seckill.getName());
         System.out.println(seckill);
     }
 
-    public void testQueryAll() throws Exception{
-        long id = 1000;
-        Seckill seckill = seckillDao.queryById(id);
-        System.out.println();
+    @Test
+    public void queryAll() throws Exception {
+
+        List<Seckill> seckills=seckillDao.queryAll(0,100);
+        for (Seckill seckill : seckills)
+        {
+            System.out.println(seckill);
+        }
     }
 
-    public void testReduceNumber() throws Exception{
-        long id = 1000;
-        Seckill seckill = seckillDao.queryById(id);
-        System.out.println();
+    @Test
+    public void reduceNumber() throws Exception {
+
+        long seckillId=1000;
+        Date date=new Date();
+        int updateCount=seckillDao.reduceNumber(seckillId,date);
+        System.out.println(updateCount);
+
     }
 
 }
---
+++ src/test/java/org/seckill/dao/SuccessKilledDaoTest.java
@@ -0,0 +1,40 @@
+package org.seckill.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.entity.SuccessKilled;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by JavaDev on 2017/4/22.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring的配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SuccessKilledDaoTest {
+
+    @Resource
+    private SuccessKilledDao successKilledDao;
+
+    @Test
+    public void insertSuccessKilled() throws Exception {
+
+        long seckillId = 1000L;
+        long userPhone = 13476191877L;
+        int insertCount = successKilledDao.insertSuccessKilled(seckillId, userPhone);
+        System.out.println("insertCount=" + insertCount);
+    }
+
+    @Test
+    public void queryByIdWithSeckill() throws Exception {
+        long seckillId = 1000L;
+        SuccessKilled successKilled = successKilledDao.queryByIdWithSeckill(seckillId);
+        System.out.println(successKilled);
+        System.out.println(successKilled.getSeckill());
+
+
+    }
+}
